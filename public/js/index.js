58ce7b157aa59822eafe951ea180475e155161dd
edwardzhong <edwardloveyou@gmail.com>
2018-06-05 14:13:13 +0800

    fix auto save

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,9 @@
+var FeedID = 0,
+    storage = new Storage('addArticle' + Suid);
 
-var FeedID=0,
-    storage=new Storage('addArticle');
-
-function restoreData(){
+function restoreData() {
     var obj = storage.get();
-    if(obj&&obj.title){
+    if (obj && obj.uid) {
         $('#title').val(obj.title);
         $('#content').html(obj.content);
         $('#editImgs').html(obj.imgs);
@@ -13,18 +12,19 @@ function restoreData(){
     }
 }
 
-function autoSave(){
-    function save(){
-        var title=$.trim($('#title').val()),
-            content=$.trim($('#content').html()),
-            imgs=$('#editImgs').html(),
-            tags=$.trim($('#tag').val());
-        if(title&&content){
+function autoSave() {
+    function save() {
+        var title = $.trim($('#title').val()),
+            content = $.trim($('#content').html()),
+            imgs = $('#editImgs').html(),
+            tags = $.trim($('#tag').val());
+        if (title && content) {
             storage.save({
-                title:title,
-                content:content,
-                imgs:imgs,
-                tags:tags
+                uid: Suid,
+                title: title,
+                content: content,
+                imgs: imgs,
+                tags: tags
             });
         }
         setTimeout(save, 50000);
@@ -44,7 +44,7 @@ function bindEvents() {
             that.parent().remove();
         });
     });
-    $('#addTag').on('click',function(e){
+    $('#addTag').on('click', function(e) {
         $('#tag').toggle();
     });
 
@@ -66,8 +66,8 @@ function bindEvents() {
     $('#replyComment').on('click', function(e) {
         e.stopPropagation();
         // todo: 评论提交
-        var commentText=$('#commentText');
-        if(!$.trim(commentText.html())){
+        var commentText = $('#commentText');
+        if (!$.trim(commentText.html())) {
             commentText.addClass('flash');
             setTimeout(function() {
                 commentText.removeClass('flash');
@@ -78,16 +78,16 @@ function bindEvents() {
         $('#overlay').hide();
         $('#comment').hide();
         sendFn({
-            r:'/feed/review',
-            feed_id:FeedID,
-            content:commentText.html()
-        },function(ret){
+            r: '/feed/review',
+            feed_id: FeedID,
+            content: commentText.html()
+        }, function(ret) {
             $('#loading').hide();
-            if(ret.code == 200){
+            if (ret.code == 200) {
                 location.reload();
                 console.log(ret);
             } else {
-                showDialog({txt:ret.msg,confirm:'确认'});  
+                showDialog({ txt: ret.msg, confirm: '确认' });
             }
         });
     });
@@ -111,133 +111,133 @@ function bindEvents() {
     //文章提交事件
     $('#submit').on('click', function() {
         // todo: 文章表单提交
-        var title=$('#title'),
-            content=$('#content'),
-            imgs=[],
-            tags=$('#tag').val();
+        var title = $('#title'),
+            content = $('#content'),
+            imgs = [],
+            tags = $('#tag').val();
 
-        if(!$.trim(title.val())){
+        if (!$.trim(title.val())) {
             title.addClass('warn');
-            showDialog({txt:'标题不能为空',confirm:'确认'},function(){
+            showDialog({ txt: '标题不能为空', confirm: '确认' }, function() {
                 title.focus();
             });
             return;
         }
         title.removeClass('warn');
 
-        if(!$.trim(content.html())){
+        if (!$.trim(content.html())) {
             content.addClass('warn');
-            showDialog({txt:'内容不能为空',confirm:'确认'},function(){
+            showDialog({ txt: '内容不能为空', confirm: '确认' }, function() {
                 content.focus();
             });
             return;
         }
         content.removeClass('warn');
-        $('#editImgs').find('img').each(function(i,item){
+        $('#editImgs').find('img').each(function(i, item) {
             imgs.push(item.src);
         });
         $('#loading').show();
         sendFn({
-            r:'/user/publish',
-            title:title.val(),
-            content:content.html(),
-            img_url:imgs.join(','),
-            tags:tags
-        },function(ret){
+            r: '/user/publish',
+            title: title.val(),
+            content: content.html(),
+            img_url: imgs.join(','),
+            tags: tags
+        }, function(ret) {
             $('#loading').hide();
             storage.clear();
-            if(ret.code == 200){
+            if (ret.code == 200) {
                 location.reload();
             } else {
-                showDialog({txt:ret.msg,confirm:'确认'});
+                showDialog({ txt: ret.msg, confirm: '确认' });
             }
         });
     });
 
     $('.article-list,.view-article').on('click', '[data-tag]', function(e) {
         e.stopPropagation();
-        var that=$(this);
-            name = that.data('tag'),
-            fid =that.data('id'),
+        var that = $(this);
+        name = that.data('tag'),
+            fid = that.data('id'),
             opts = {};
 
         switch (name) {
-            case 'up'://点赞
+            case 'up': //点赞
                 sendFn({
-                    r:'/feed/digg',
-                    feed_id:fid
-                },function(ret){
+                    r: '/feed/digg',
+                    feed_id: fid
+                }, function(ret) {
                     $('#loading').hide();
-                    if(ret.code == 200){
+                    if (ret.code == 200) {
                         that.toggleClass('active');
                         console.log(ret);
                     } else {
-                        showDialog({txt:ret.msg,confirm:'确认'});  
+                        showDialog({ txt: ret.msg, confirm: '确认' });
                     }
                 });
                 break;
             case 'more': //更多图标
                 hideDialogs();
-                var menu=$(this).siblings('[data-menu=more]');
-                showDownMenu(menu,$(this));
+                var menu = $(this).siblings('[data-menu=more]');
+                showDownMenu(menu, $(this));
                 break;
             case 'share': //分享图标
                 hideDialogs();
-                var menu=$(this).siblings('[data-menu=share]');
-                showDownMenu(menu,$(this));
+                var menu = $(this).siblings('[data-menu=share]');
+                showDownMenu(menu, $(this));
                 break;
             case 'delete': //删除事件
                 var that = $(this);
                 showDialog('确定删除?', function() {
                     // todo: 删除
                     sendFn({
-                        r:'/user/del',
-                        feed_id:fid
-                    },function(ret){
-                        if(ret.code == 200){
+                        r: '/user/del',
+                        feed_id: fid
+                    }, function(ret) {
+                        if (ret.code == 200) {
                             if (that.parents('ul').length) {
                                 that.parentsUntil('ul').remove();
                             }
                         } else {
-                            showDialog({txt:ret.msg,confirm:'确认'});  
+                            showDialog({ txt: ret.msg, confirm: '确认' });
                         }
                     });
                 });
                 break;
             case 'private': //私密
                 hideDialogs();
-                var that=$(this),
-                    pri=that.parentsUntil('.actions').siblings('.private'),
-                    showType=Number(that.data('type'));
+                var that = $(this),
+                    pri = that.parentsUntil('.actions').siblings('.private'),
+                    showType = Number(that.data('type'));
                 //todo:私密
                 sendFn({
-                    r:'/feed/set-feed-secret',
-                    feed_id:fid,
-                    show_type:3-showType
-                },function(ret){
-                    if(ret.code == 200){
+                    r: '/feed/set-feed-secret',
+                    feed_id: fid,
+                    show_type: 3 - showType
+                }, function(ret) {
+                    if (ret.code == 200) {
                         pri.toggle();
-                        that.data('type',3-showType);
-                        that.html(['设为公开','设为私密'][showType-1]);
+                        that.data('type', 3 - showType);
+                        that.html(['设为公开', '设为私密'][showType - 1]);
                     } else {
-                        showDialog({txt:ret.msg,confirm:'确认'});  
+                        showDialog({ txt: ret.msg, confirm: '确认' });
                     }
                 });
                 break;
             case 'collect': //收藏
                 // todo: 收藏
                 hideDialogs();
-                showDialog({txt:'已收藏',confirm:'确认'});
+                showDialog({ txt: '已收藏', confirm: '确认' });
                 break;
             case 'copy': //复制
                 break;
             case 'comment': //评论
-                var wrap=$(this).parents('.dream-wrap'),
+                var wrap = $(this).parents('.dream-wrap'),
                     userNames = wrap.find('.title span'),
-                    l=wrap.offset().left,
-                    w=wrap.width();
-                showComment(userNames,l,w);
-                FeedID=fid;
+                    l = wrap.offset().left,
+                    w = wrap.width();
+                showComment(userNames, l, w);
+                FeedID = fid;
                 break;
                 /**
                  * sns分享
@@ -275,6 +275,4 @@ function bindEvents() {
 uploadImage();
 setCopyUrl();
 bindEvents();
-scrollPage({r:'/user/get-user-home',pagehas_img:HasImg||''});
-
-
+scrollPage({ r: '/user/get-user-home', pagehas_img: HasImg || '' });
